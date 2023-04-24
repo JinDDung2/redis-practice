e233ab5c1a825e9086443118e590e0276fae8fac
Jin-hyuck <gjwlsgur4866@gmail.com>
2023-04-24 16:24:24 +0900

    first commit

---
+++ src/test/java/com/example/gameboard/GameboardApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.gameboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
